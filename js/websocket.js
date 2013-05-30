d5681e25102decc94e326076c5b4f5ca91556404
iuuso <juuso.karlstrom@gmail.com>
2013-05-30 13:02:19 +0300

    temp backup

--- js/websocket.js
+++ js/websocket.js
@@ -14,6 +14,7 @@ function connect() {
 
     websocket.onmessage = receiveOutput;
     websocket.onerror = onError;
+
     } 
     else {
       alert("WebSockets not supported on your browser.");
@@ -30,22 +31,21 @@ var serverStatus; // Variable for server status (Connected/Disconnected/Error)
 
 function init(){
   serverStatus = document.getElementById("serverStatus");
-  serverStatus.innerHTML = "<p class='text-info'>NOT CONNECTED</p>";
+  //serverStatus.innerHTML = "<p class='text-info'>NOT CONNECTED</p>";
 } // end init
 
 function onOpen(evt){
   //called as soon as a connection is opened
-  serverStatus.innerHTML = "<p class='text-success'>CONNECTED TO SERVER</p>";
+  sendCommand();
 } // end onOpen
 
 function onClose(evt){
   //called when connection is severed
-  serverStatus.innerHTML = "<p class = 'text-error'>DISCONNECTED</p>";
+  //serverStatus.innerHTML = "<p class = 'text-error'>DISCONNECTED</p>";
 } // end onClose;
 
 function onError(evt){
-  //called on error
-  serverStatus.innerHTML += "<p class='error'>ERROR: " + evt.data + "</p>";
+  window.alert("Error establishing WebSocket-connection to server" + evt.data)
 } // end onError
 
 
@@ -59,19 +59,20 @@ function sendCommand() {
 
   outputCommand = document.getElementById("outputCommand");
 
-  commandBase = "tshark -n -T fields -E separator=, -e frame.number -e ip.src -e ip.dst -c 5 -i en1 src host ";
-  ipAddress = "10.20.211.179"
+  commandBase = "tshark -n -T fields -E separator=, -e frame.number -e ip.src -e ip.dst -c 3 -i en1 src host ";
+  ipAddress = "10.20.45.124"
   command = commandBase + ipAddress;
   websocket.send(command);
   outputCommand.innerHTML += "<p class='text-info'>Command sent: " + command + "</p>";
 
 }
 
+var table = document.getElementById('outputTable');
+
 function receiveOutput(evt){
 
     // Table creation
 
-    var table = document.getElementById("outputTable");
     var body = document.createElement('tbody');
 
     outputxml = evt.data;
@@ -88,6 +89,9 @@ function receiveOutput(evt){
         // pDetails[0] == Frame number
         // pDetails[1] == Source ip
         // pDetails[2] == Destination ip
+
+ 
+
 
         // Create row for the table
         var row = document.createElement("tr");
@@ -112,36 +116,22 @@ function receiveOutput(evt){
 
 
           // Cell for City, Country
-          var locationCell = document.createElement("th");
-          var locationCelltext = document.createTextNode("F-F-Fetching...");
+          var locationCell = document.createElement("th")
+          var locationCelltext = document.createTextNode("(location)" + pDetails[0]);
           locationCell.appendChild(locationCelltext);
 
           // Cell for latitude
           var latitudeCell = document.createElement("th");
-          var latitudeCelltext = document.createTextNode("F-F-Fetching...");
+          var latitudeCelltext = document.createTextNode("(latitude)" + pDetails[0]);
           latitudeCell.appendChild(latitudeCelltext);
 
           //Cell for longitude
           var longitudeCell = document.createElement("th");
-          var longitudeCelltext = document.createTextNode("F-F-Fetching...")
+          var longitudeCelltext = document.createTextNode("latitude)" + pDetails[0]);
           longitudeCell.appendChild(longitudeCelltext)
 
 
-              // This is a WorldIP free geo-location database.
-              var freegeoip = "http://freegeoip.net/json/";
 
-              // Attach outgoing ip to WorldIP json
-              var url = freegeoip + pDetails[2];
-
-              // http://robertodecurnex.github.io/J50Npi/
-              var data = {};
-              var callback = function(geodata){               
-                //locationCell.innerHTML = (geodata.city_name + ", " + geodata.country_name)
-                locationCell.innerHTML += "<p class='text-info'>" + geodata.country_name + "</p>";
-                latitudeCell.innerHTML = (geodata.latitude)
-                longitudeCell.innerHTML = (geodata.longitude)
-              }
-              J50Npi.getJSON(url, data, callback);
 
 
         // Attach cells to row
@@ -152,17 +142,90 @@ function receiveOutput(evt){
         row.appendChild(latitudeCell);
         row.appendChild(longitudeCell);
 
-
-
+        
          // Attach row to body
         body.appendChild(row);  
-}
-
+    }
     table.appendChild(body)
+
+    GetCellValues();
+
 
 } // end onMessage
 
+   function GetCellValues() {
+
+        var freegeoip = "http://freegeoip.net/json/";
+
+        for (var r = 1, n = table.rows.length; r < n; r++) {
+
+            for (var c = 2, m = 3; c < m; c++) {
+
+                //alert(table.rows[r].cells[c].innerHTML);
+                var url = freegeoip + table.rows[r].cells[c].innerHTML;
+
+                // http://robertodecurnex.github.io/J50Npi/
+                var data = {};
+                callback = function(geodata){           
+                    window.alert("funktion sisällä: " + geocountry)
+                }
+               J50Npi.getJSON(url, data, callback);
+
+            }
+        }
+    }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       // // Locate ip
+
+       //        // This is a WorldIP free geo-location database.
+       //        var freegeoip = "http://freegeoip.net/json/";
+
+       //        // Attach outgoing ip to WorldIP json
+       //        var url = freegeoip + destinationCelltext.value;
+
+       //        // http://robertodecurnex.github.io/J50Npi/
+       //        var data = {};
+       //        var callback = function(geodata){
+       //          window.alert(geodata.country_name)
+       //          //locationCell.innerHTML = (geodata.city_name + ", " + geodata.country_name)
+       //          //latitudeCell.innerHTML = (geodata.latitude)
+       //          //longitudeCell.innerHTML = (geodata.longitude)
+                
+       //        }
+       //        J50Npi.getJSON(url, data, callback);
+       //        window.alert("GEODATA : " + data)
+       //        console.log(data)
+       //        var country = JSON.stringify(data)
+       //        console.log(data)
+       //  // End locate ip
 
 
