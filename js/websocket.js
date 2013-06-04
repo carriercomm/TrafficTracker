8089d4940a57287bceb0306cc288ab17193b1b75
iuuso <juuso.karlstrom@gmail.com>
2013-06-04 12:26:31 +0300

    FINALLY managed to create a properly working table
    Seems like that the answer was mystery that is called "global variable"

--- js/websocket.js
+++ js/websocket.js
@@ -65,21 +65,54 @@ function sendCommand() {
   var packetAmount = document.getElementById("amountOfPackets").value
   var temp1 = "-c 50"// + packetAmount; // Temp value, because we need this variable to create tabl
   //ipAddress = " src host " + document.getElementById("ipInput").value
-  ipAddress = " src host 10.20.216.77"
+  ipAddress = " src host 10.20.214.53"
   command = commandBase + temp1 + ipAddress;
   websocket.send(command);
+  createRows();
 }
 
+// Table elements
 var table = document.getElementById('outputTable');
 var body = document.createElement('tbody')
-//var row = document.createElement("tr");
+
+
+var frameCell         // Frame number
+var sourceCell        // Source IP
+var destinationCell   // Destination IP
+
+function createRows() {
+
+  for(var r=0; r<50; r++) {
+
+  var row = body.insertRow(r);
+
+    frameCell = row.insertCell(0)
+    frameCell.setAttribute("id", "frameCell" + r )
+    frameCell.innerHTML = "null"
+
+    sourceCell = row.insertCell(1)
+    sourceCell.setAttribute("id", "sourceCell" + r )
+    sourceCell.innerHTML = "null"
+
+    destinationCell = row.insertCell(2)
+    destinationCell.setAttribute("id", "destinationCell" + r)
+    destinationCell.innerHTML = "null"
+
+    locationCell = row.insertCell(3)
+    locationCell.setAttribute("id", "locationCell" + r)
+    locationCell.innerHTML = "null"
+
+    body.appendChild(row)
+  }
+
+  table.appendChild(body)
+
+}
 
 function receiveOutput(evt){
 
     outputxml = evt.data;
     outputxml.toString();
-
-    //var row = document.createElement("tr");
     
     // First let's split the data for per packets
     var packet = outputxml.split(/\n/)
@@ -94,28 +127,27 @@ function receiveOutput(evt){
         // pDetails[1] == Source ip
         // pDetails[2] == Destination ip
 
-        // Create row for the table
+        // var keijo = pDetails[2].indexOf("130.231.241.29")
 
-        var newRow = body.insertRow(i);
+        //   if (keijo == '-1') {
+        //     console.log("Keijo ei löytänyt samaa IP-osoitetta")
+        //   } else
+        //     console.log("Keijo löysi saman IP-osoitteen : " + keijo )        
 
-        // Frame number
-        var newCell0 = newRow.insertCell(0)      
-        newCell0.innerHTML = pDetails[0];
+        var frame = "frameCell" + i    
+        var frameCell = document.getElementById(frame).innerHTML = pDetails[0]
 
-        // Source ip
-        var newCell1 = newRow.insertCell(1)
-        newCell1.innerHTML = pDetails[1]
+        var source = "sourceCell" + i
+        var frameCell = document.getElementById(source).innerHTML = pDetails[1]
 
-        // Destination ip
-        var newCell2 = newRow.insertCell(2)
-        newCell2.innerHTML = pDetails[2]
-
+        var destination = "destinationCell" + i
+        var destinationCell = document.getElementById(destination).innerHTML = pDetails[2]
         
     }
 
     // Attach row to body
-    body.appendChild(newRow)
-    table.appendChild(body) 
+    //body.appendChild(newRow)
+    //table.appendChild(body) 
 
     console.log("Values fetched, getting cell values")
 
@@ -128,11 +160,13 @@ function receiveOutput(evt){
 
 var locationTable = document.getElementById("locTable")
 var locationBody = document.createElement("tbody")
+var kalle;  // Variable for loop counter in callback-function
+kalle = 0;  
 
 function GetCellValues() {
 
     var freegeoip = "http://freegeoip.net/json/";
-    var newRow = document.createElement("tr");
+    //var newRow = document.createElement("tr");
 
     for (var r = 1, n = table.rows.length; r < n;r++) {
 
@@ -144,24 +178,21 @@ function GetCellValues() {
             // http://robertodecurnex.github.io/J50Npi/
             var data = {};
               callback = function(geodata){
-                    var newRow = locationBody.insertRow(-1);
-                    
-                    var destinationCell = newRow.insertCell(0)
-                    var locationCell = newRow.insertCell(1)
-                    var latitudeCell = newRow.insertCell(2)
-                    var longitudeCell = newRow.insertCell(3)
 
-                    destinationCell.innerHTML = geodata.ip                    
-                    locationCell.innerHTML = geodata.city + ", " + geodata.country_name
-                    latitudeCell.innerHTML = geodata.latitude
-                    longitudeCell.innerHTML = geodata.longitude
+                  kalle++
+                  console.log("Muuttuja Kalle : " + kalle)
+
+
+                    var location = "locationCell" + kalle
+                    var locationCell = document.getElementById(location).innerHTML = geodata.country_name
+
 
               }
            J50Npi.getJSON(url, data, callback);
         }
   }
-  locationBody.appendChild(newRow)
-  locationTable.appendChild(locationBody)
+  //locationBody.appendChild(newRow)
+  //locationTable.appendChild(locationBody)
 console.log("Location values done, calling for GMaps")
 }
 
