3af8ae8c7f9a3178e705513f57af0dabb9c17218
iuuso <juuso.karlstrom@gmail.com>
2013-06-07 14:59:28 +0300

    Safe commit

--- js/websocket.js
+++ js/websocket.js
@@ -99,119 +99,132 @@ var body = document.createElement('tbody')
 //-------------------------------------------------------------
 
 
-
-var justStupidCounter;  // Variable for loop counter in callback-function
-justStupidCounter = 0; 
-
-
-
-//-------------------------------------------------------------
-
-
-
 var cityArray = []
 var addressArray = []
 var countryArray = []
 var latitudeArray = []
 var longitudeArray = []
 
+var destinationArray = []
 var duplicateCount = []
-
+var justStupidCounter; 
+justStupidCounter = 0; 
 
 
 //-------------------------------------------------------------
 
-var pee = 1;
 
 function receiveOutput(evt){
 
-  // pitäskö tähän laittaa joku looppi?
+  console.log("justStupidCounter : " + justStupidCounter)
+
+    // evt.data is the data received from server
     outputxml = evt.data;
     outputxml.toString();
     
-    // First let's split the data for per packets
+    // split data per packet
     var packet = outputxml.split(/\n/)
   
       // Print packets line by line
       for (var i=0;i<packet.length-1;i++) {
 
         var row = body.insertRow(i);
-        //row.setAttribute("class", "success")
+
+        var pDetails = packet[i].split(",") 
 
 
-        var pDetails = packet[i].split(",") // Split every packet separately
 
         // pDetails[0] == Frame number
         // pDetails[1] == Source ip
-        // pDetails[2] == Destination ip     
-
-          frameCell = row.insertCell(0)
-          frameCell.setAttribute("id", "frameCell" + justStupidCounter )
-          frameCell.innerHTML = pDetails[0]
-          
-
-          sourceCell = row.insertCell(1)
-          sourceCell.setAttribute("id", "sourceCell" + justStupidCounter )
-          if ( pDetails[1] == "" ) {
-            sourceCell.innerHTML ="null"
-          } else 
-          sourceCell.innerHTML = pDetails[1]
-
-          destinationCell = row.insertCell(2)
-          destinationCell.setAttribute("id", "destinationCell" + justStupidCounter )
-          if ( pDetails[2] == "") {
-            destinationCell.innerHTML = "null"
-          } else
-          destinationCell.innerHTML = pDetails[2]
-
-          locationCell = row.insertCell(3)
-          locationCell.setAttribute("id", "locationCell" + justStupidCounter )
-          locationCell.innerHTML = "F-f-f-fetching...."
-          console.log("locationCell" + justStupidCounter)
+        // pDetails[2] == Destination ip  
 
 
-          latitudeCell = row.insertCell(4)
-          latitudeCell.setAttribute("id", "latitudeCell" + justStupidCounter )
-          latitudeCell.innerHTML = "disabled"
 
-          longitudeCell = row.insertCell(5)
-          longitudeCell.setAttribute("id", "longitudeCell" + justStupidCounter )
-          longitudeCell.innerHTML = "disabled"
+        // Filter duplicates from the destination array
+        if (destinationArray.indexOf(pDetails[2]) != "-1" ) {
+
+          console.log("Burn the Duplicate! Counter : " + duplicateCount.length)
+          duplicateCount.push(1)
+
+        }  else
+
+              // Cell for packet number
+              frameCell = row.insertCell(0)
+              frameCell.setAttribute("id", "frameCell" + justStupidCounter )
+              frameCell.innerHTML = pDetails[0]
+
+              // Cell for source IP
+              sourceCell = row.insertCell(1)
+              sourceCell.setAttribute("id", "sourceCell" + justStupidCounter )
+              if ( pDetails[1] == "" ) {
+                sourceCell.innerHTML ="null"
+              } else 
+              sourceCell.innerHTML = pDetails[1]
+
+              // Cell for destination IP
+              destinationCell = row.insertCell(2)
+              destinationCell.setAttribute("id", "destinationCell" + justStupidCounter )
+              if ( pDetails[2] == "") {
+                destinationCell.innerHTML = "null"
+              } else
+              destinationCell.innerHTML = pDetails[2]
+
+              destinationArray.push(pDetails[2])
 
 
-        body.appendChild(row)
-        table.appendChild(body)
+
+              // Cells for location information
+
+                // Cell for location
+                locationCell = row.insertCell(3)
+                locationCell.setAttribute("id", "locationCell" + justStupidCounter )
+                locationCell.innerHTML = "F-f-f-fetching...."
+
+                // Cell for latitude
+                latitudeCell = row.insertCell(4)
+                latitudeCell.setAttribute("id", "latitudeCell" + justStupidCounter )
+                latitudeCell.innerHTML = "disabled"
+
+                // Cell for longitude
+                longitudeCell = row.insertCell(5)
+                longitudeCell.setAttribute("id", "longitudeCell" + justStupidCounter )
+                longitudeCell.innerHTML = "disabled"
+
+              // END for location information
 
 
-          var freegeoip ="http://freegeoip.net/json/"
-          var url = freegeoip + pDetails[2]
 
-          // Credit for this function to Roberto Decurnex
-          // http://robertodecurnex.github.io/J50Npi/
-          var data = {};
-          callback = function(geodata) {
+            body.appendChild(row)
+            table.appendChild(body)
 
-            // Store locationdata in different arrays
 
-            // if geodata ip on jo in array
-            // then dont push anything
-            // else push kaikki
-            if (addressArray.indexOf(geodata.ip) != "-1") {
-              duplicateCount.push(1)
-              console.log("Hey, i think we have a duplicate! " + duplicateCount.length)
-              
-            } else
-            addressArray.push(geodata.ip)
-            countryArray.push(geodata.country_name)  
-            latitudeArray.push(geodata.latitude)
-            longitudeArray.push(geodata.longitude)
-            
-          }
-          J50Npi.getJSON(url, data, callback);
 
-          justStupidCounter++ // Kasvaa oikein, per paketti
+            // Fetch location information based on destination IP
+
+              var freegeoip ="http://freegeoip.net/json/"
+              var url = freegeoip + pDetails[2]
+
+              // Credit for this function to Roberto Decurnex
+              // http://robertodecurnex.github.io/J50Npi/
+              var data = {};
+              callback = function(geodata) {
+
+                addressArray.push(geodata.ip)
+                cityArray.push(geodata.city)
+                countryArray.push(geodata.country_name)  
+                latitudeArray.push(geodata.latitude)
+                longitudeArray.push(geodata.longitude)
+                
+              }
+              J50Npi.getJSON(url, data, callback);
+
+              justStupidCounter++
+
+            // END location fetching
         
     }
+
+    locations()
 
 } // end receiveOutput
 
@@ -226,7 +239,6 @@ var failureCount = []
 function locations() {
 
 
-
   for (e=0;e<=addressArray.length-1; e++) {
 
     var temp1 = "locationCell" + e
@@ -234,18 +246,18 @@ function locations() {
     var temp3 = "longitudeCell" + e
     var temp4 = "destinationCell" + e 
 
-    if (addressArray[e] == "Reserved") {
+    if (countryArray[e] == "Reserved") {
  
       var locationCell = document.getElementById(temp1).innerHTML = "<a href='http://en.wikipedia.org/wiki/Reserved_IP_addresses' target='_blank'>Reserved address</a>"
+
    
     } else {
 
-        var locationCell = document.getElementById(temp1).innerHTML =  addressArray[e] + " | " + cityArray[e] + ", " + countryArray[e]
+        var locationCell = document.getElementById(temp1).innerHTML =  /*addressArray[e] + " | " + */ cityArray[e] + ", " + countryArray[e]
         var latitudeCell = document.getElementById(temp2).innerHTML = latitudeArray[e]
         var longitudeCell = document.getElementById(temp3).innerHTML = longitudeArray[e]
 
         var destIP = document.getElementById(temp4).innerHTML
-        console.log(destIP)
 
   }
 
